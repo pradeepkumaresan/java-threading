97ddaa7e9d16620a49a82f63541c3f03880dc703
Pradeep Kumaresan <pradeep.it39@gmail.com>
2020-05-12 23:34:55 +0530

    bug fixes

--- src/main/java/com/pradeep/samplewebserver/controllers/SampleController.java
+++ src/main/java/com/pradeep/samplewebserver/controllers/SampleController.java
@@ -2,7 +2,6 @@ package com.pradeep.samplewebserver.controllers;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.CountDownLatch;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +10,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class SampleController {
 
-	private static final Map<Long, CountDownLatch> map = new HashMap<>();
+	private static final Map<Long, Object> map = new HashMap<>();
 	private static final Map<Long, Object> syncMap = new HashMap<>();
 
 	@GetMapping("/test/{id}")
 	public void sampleRequest(@PathVariable Long id) {
 		syncMap.putIfAbsent(id, id);
 		synchronized (syncMap.get(id)) {
-
 			if (!map.containsKey(id)) {
-				map.putIfAbsent(id, new CountDownLatch(1));
+				map.putIfAbsent(id, new Object());
 				System.out.println(id + " initial call start");
 				// processing method call here
 				try {
@@ -29,22 +27,11 @@ public class SampleController {
 					e.printStackTrace();
 				}
 				System.out.println(id + " initial call end");
-				map.get(id).countDown();
-
 			}
 		}
 		if (map.containsKey(id)) {
-			System.out.println(id + " subsequent call start");
-			try {
-				map.get(id).await();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			System.out.println(id + " subsequent call end");
+			System.out.println(id + " subsequent call");
 			// processing method call here
-
 		}
-
 	}
-
 }
