98db52957ac0bb79b36b8910389720d255fd2986
Pradeep Kumaresan <pradeep.it39@gmail.com>
2020-05-12 22:51:59 +0530

    more changes

--- src/main/java/com/pradeep/samplewebserver/controllers/SampleController.java
+++ src/main/java/com/pradeep/samplewebserver/controllers/SampleController.java
@@ -11,35 +11,39 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class SampleController {
 
-	private static Map<Long, CountDownLatch> map = new HashMap<>();
+	private static final Map<Long, CountDownLatch> map = new HashMap<>();
+	private static final Map<Long, Object> syncMap = new HashMap<>();
 
 	@GetMapping("/test/{id}")
 	public void sampleRequest(@PathVariable Long id) {
-		if (!map.containsKey(id)) {
-			map.putIfAbsent(id, new CountDownLatch(1));
-			System.out.println(id + " first call start");
-			//processing method call here
-			try {
-				Thread.sleep(3000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		syncMap.putIfAbsent(id, id);
+		synchronized (syncMap.get(id)) {
+
+			if (!map.containsKey(id)) {
+				map.putIfAbsent(id, new CountDownLatch(1));
+				System.out.println(id + " initial call start");
+				// processing method call here
+				try {
+					Thread.sleep(id * 1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println(id + " initial call end");
+				map.get(id).countDown();
+
 			}
-			System.out.println(id + " first call end");
-			map.get(id).countDown();
-			
 		}
-		else {
+		if (map.containsKey(id)) {
+			System.out.println(id + " subsequent call start");
 			try {
 				map.get(id).await();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			System.out.println(id + " subsequent");
-			//processing method call here
-			
+			System.out.println(id + " subsequent call end");
+			// processing method call here
+
 		}
-		
 
 	}
 
